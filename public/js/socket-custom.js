45d279f3bad7b55fbb307dc8f89636d8871e99bc
Noe <noeliasebastian@gmail.com>
2019-09-01 21:18:40 +0200

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,22 @@
+var socket = io()
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+})
+
+// Escuchar
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+})
+
+// Enviar informacion
+socket.emit('enviarMensaje', {
+    usuario: 'Noelia',
+    mensaje: 'Hola mundo'
+}, function(resp) {
+    console.log('Respuesta server ', resp);
+})
+
+// Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+    console.log('Mensaje desde el servidor: ', mensaje);
+})
